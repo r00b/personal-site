fa8eb24b821bc2de6f6c305a6f74622c24d3b152
Robert Steilberg <rsteilberg@esri.com>
2020-09-17 11:37:20 -0400

    kill resize

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,7 +14,7 @@ const Layout = ({ children }) => {
       document.documentElement.style.setProperty("--vh", `${viewportHeight}px`);
     };
     setViewportHeight();
-    window.addEventListener("resize", setViewportHeight);
+    // window.addEventListener("resize", setViewportHeight);
   }
 
   const data = useStaticQuery(graphql`
