2ae5b72b4671ecf4537d81511b88582810fb69a8
Robert Steilberg <rsteilberg@esri.com>
2020-09-17 16:45:08 -0400

    use resize listener and viewport-fit=containt

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,7 +14,7 @@ const Layout = ({ children }) => {
       document.documentElement.style.setProperty("--vh", `${viewportHeight}px`);
     };
     setViewportHeight();
-    window.addEventListener("orientationchange", setViewportHeight);
+    window.addEventListener("resize", setViewportHeight);
   }
 
   const data = useStaticQuery(graphql`
