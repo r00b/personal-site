8d929dbba00fc50c6a2101d2024e538adbd4e810
Rob Steilberg <rsteilberg@gmail.com>
2020-09-19 13:18:52 -0400

    Mobile tailwind (#3)
    * js -> jsx
    
    * CSS tweaks and fixes
    
    * finish mobile optimizations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,26 +3,40 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
+  purge: ["./src/**/*.js", "./src/**/*.jsx"],
   theme: {
+    fontFamily: {
+      serif: [
+        '"EB Garamond"',
+        "Georgia",
+        "Cambria",
+        '"Times New Roman"',
+        "Times",
+        "serif",
+      ],
+    },
+    boxShadow: {
+      default:
+        "0 10px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);",
+    },
     extend: {
       animation: {
-        'cycle': 'cycle 20s linear infinite 0s'
+        cycle: "cycle 20s linear infinite 0s",
       },
       keyframes: {
         cycle: {
-          '0%': { opacity: '0', transform: 'translateY(0px)' },
-          '1%': { opacity: '1', transform: 'translateY(0px)' },
-          '22%': { opacity: '1', transform: 'translateY(0px)' },
-          '25%': { opacity: '0', transform: 'translateY(50px)' },
-          '100%': { opacity: '0', transform: 'translateY(50px)' }
-        }
-      }
+          "0%": { opacity: "0", transform: "translateY(0px)" },
+          "1%": { opacity: "1", transform: "translateY(0px)" },
+          "22%": { opacity: "1", transform: "translateY(0px)" },
+          "25%": { opacity: "0", transform: "translateY(50px)" },
+          "100%": { opacity: "0", transform: "translateY(50px)" },
+        },
+      },
+      colors: {
+        "sky-blue": "#285d81",
+      },
     },
-    boxShadow: {
-      default: '0 10px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);'
-    }
   },
   variants: {},
   plugins: [],
-}
+};
