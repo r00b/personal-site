763fee2f7d65477ac150d3de31af880f0a125a76
Robert Steilberg <rsteilberg@gmail.com>
2026-01-24 12:20:04 -0600

    remove boeing

--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -32,7 +32,7 @@ const Avatar = () => {
           <h2>
             senior software development
             <br />
-            engineer, ForeFlight / Boeing
+            engineer, Jeppesen ForeFlight
           </h2>
         </div>
       </div>
